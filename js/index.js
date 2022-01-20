e613f31367b19ce9b4cf3a30baae6a4eb82fcad1
Nazar <thepsix007@gmail.com>
2022-01-20 18:44:14 +0200

    statistic

--- js/index.js
+++ js/index.js
@@ -1,53 +1,63 @@
-let template = document.querySelector('#js-user')
-let parent = document.querySelector('.js-users')
-let btn = document.querySelector('.js-btn')
-let preloader = document.querySelector('.js-preloader')
+const parent = document.querySelector('.js-users')
+const btn = document.querySelector('.js-btn')
+const preloader = document.querySelector('.js-preloader')
+const template = document.querySelector('#js-user')
+
 
 btn.addEventListener('click', () => {
-  preloader.classList.remove('preloader--hide')
   const promise = getUsers(randomInteger(1, 100)) // Делаем запрос на сервер || Получаем промис
   promise.then(response => response.json()).then(data => createUsers(data))
 
-  btn.remove()
+  switchPreloader(true)
+  btn.classList.add('btn--hidden')
 })
 
-function randomInteger(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
-}
-
 function createUsers(data) {
-  preloader.remove()
+  let frag = document.createDocumentFragment()
 
   data.results.forEach(elem => {
-    template.content.querySelector('.js-user-card__img').src = elem.picture.large
-    template.content.querySelector('.js-user-card__name').textContent = ''
-
-    for (let key in elem.name) {
-      template.content.querySelector('.js-user-card__name').textContent += ' ' + elem.name[key]
-    }
-
-    template.content.querySelector('.js-user-card__gender').textContent = elem.gender
-    template.content.querySelector('.js-user-card__gender').dataset.gender = elem.gender
-
-    template.content.querySelector('.js-user-card__number').textContent = elem.phone
-    template.content.querySelector('.js-user-card__number').href = 'tel:' + elem.phone
-
-    template.content.querySelector('.js-user-card__email').textContent = elem.email
-    template.content.querySelector('.js-user-card__email').href = 'mailto:' + elem.email
-
-    template.content.querySelector('.js-user-card__location div').textContent = `${elem.location.city}, ${elem.location.street.name},`
-
-    template.content.querySelector('.js-user-card__location span').textContent = elem.location.street.number
-
-    let birthday = new Date(elem.dob.date)
-    template.content.querySelector('.js-user-card__birthday span').textContent = `${birthday.getDate()}-${birthday.getMonth()}-${birthday.getFullYear()}`
-
-    let reg = new Date(elem.registered.date)
-    template.content.querySelector('.js-user-card__reg span').textContent = `${reg.getDate()}-${reg.getMonth()}-${reg.getFullYear()}`
-
+    switchPreloader(false)
 
     let user = template.content.cloneNode(true)
-    parent.appendChild(user)
+
+    user.querySelector('.js-user-card__img').src = elem.picture.large
+    user.querySelector('.js-user-card__name').textContent = ''
+    for (let key in elem.name) {
+      user.querySelector('.js-user-card__name').textContent += ' ' + elem.name[key]
+    }
+    user.querySelector('.js-user-card__gender').textContent = elem.gender
+    user.querySelector('.js-user-card__gender').dataset.gender = elem.gender
+    user.querySelector('.js-user-card__number').textContent = elem.phone
+    user.querySelector('.js-user-card__number').href = 'tel:' + elem.phone
+    user.querySelector('.js-user-card__email').textContent = elem.email
+    user.querySelector('.js-user-card__email').href = 'mailto:' + elem.email
+    user.querySelector('.js-user-card__location div').textContent = `${elem.location.city}, ${elem.location.street.name},`
+    user.querySelector('.js-user-card__location span').textContent = elem.location.street.number
+    user.querySelector('.js-user-card__birthday span').textContent = new Date(elem.dob.date).toLocaleDateString('ru')
+    user.querySelector('.js-user-card__reg span').textContent = new Date(elem.registered.date).toLocaleDateString('ru')
+
+    frag.appendChild(user)
   })
+
+  parent.appendChild(frag)
 }
+
+
+function switchPreloader(flag){
+  flag == true ? preloader.classList.remove('preloader--hidden') : preloader.classList.add('preloader--hidden')
+}
+
+function createStatistic(){
+  const statistic = document.querySelector('.js-statistic')
+  statistic.classList.remove('statistic--hidden')
+}
+
+// const state = {
+//   show(elem){
+//     return elem.classList.remove('hidden')
+//   },
+//   hide(elem){
+//     return elem.classList.add('hidden')
+//   }
+// }
 
---
+++ js/main.js
@@ -0,0 +1,3 @@
+function randomInteger(min, max) {
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
